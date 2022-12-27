3ec276c01c97600f1786170bf088fdc5bcd0c593
DJ Nelson <11001514+techied@users.noreply.github.com>
2022-12-27 12:18:52 -0800

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def run_test():
 
     results_dict = s.results.dict()
     print(results_dict)
-    with open('speedtest.csv', 'a') as f:
+    with open('/home/pi/speed-logger/speedtest.csv', 'a') as f:
         f.write(f"{results_dict['timestamp']}, {results_dict['download']}, {results_dict['upload']}, {results_dict['ping']}\n")
 
 
